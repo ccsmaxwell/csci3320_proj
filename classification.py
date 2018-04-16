70717514dce3f71b1db5eb122db11780df180928
ccsmaxwell <ccsmaxwell@link.cuhk.edu.hk>
2018-04-16 17:59:50 +0800

    complete naive_byes (3.1.2)

--- classification.py
+++ classification.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.naive_bayes import GaussianNB
+
+from naive_bayes import NaiveBayes
 
 df_train = pd.read_csv('data/training.csv')
 train_X = df_train[['actual_weight','declared_horse_weight','draw','win_odds','recent_ave_rank','jockey_ave_rank','trainer_ave_rank','race_distance']].values
@@ -17,3 +20,8 @@ nb_model = GaussianNB()
 # skf = StratifiedKFold(n_splits=10, random_state=3320, shuffle=True)
 # for train_index, test_index in skf.split(train_X, train_Y):
 nb_model.fit(train_X, train_Y)
+
+clf = NaiveBayes()
+clf = clf.fit(train_X, train_Y)
+# y_predict = clf.predict(X)
+
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,32 @@
 import numpy as np
+from scipy.stats import norm
 
+# 3.1.2
 class NaiveBayes:	
 	def fit(self, X, y):
-		
+		self.mean = np.mean(X, axis=0)
+		self.std = np.std(X, axis=0)
+
+		allClass = np.unique(y)
+
+		self.stat = {}
+		for i in allClass:
+			classX = X[y==i]
+			classStat = {}
+			classStat["prior"] = len(classX) / len(y)
+			classStat["mean"] = np.mean(classX, axis=0)
+			classStat["std"] = np.std(classX, axis=0)
+			self.stat[i] = classStat
+
+		return self
+
 	def predict(self, X):
+		result = []
+		for r in X:
+			evid = norm(self.mean, self.std).pdf(r)
+			like = dict([(c, norm(self.stat[c]['mean'], self.stat[c]['std']).pdf(r)) for c in self.stat])
+
+			post = dict([(c, self.stat[c]['prior'] * np.prod(like[c]) / np.prod(evid)) for c in self.stat])
+			result.append(max(post, key=post.get))
+
+		return result
