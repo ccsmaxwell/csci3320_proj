4a4c5ae8a1ea8a872bce7a42b83c741c040cd847
ccsmaxwell <ccsmaxwell@link.cuhk.edu.hk>
2018-04-20 12:22:15 +0800

    finish plot report

--- plot/bar_chart.py
+++ plot/bar_chart.py
@@ -7,14 +7,14 @@ from sklearn.ensemble import RandomForestClassifier
 df_train = pd.read_csv('data/training.csv')
 feature = ['actual_weight','declared_horse_weight','draw','win_odds','recent_ave_rank','jockey_ave_rank','trainer_ave_rank','race_distance']
 train_X = df_train[feature].values
-train_Y = np.ravel(df_train[['finishing_position']].values)
+train_Y = np.ravel(df_train[['finishing_position']].replace(range(2,15),0).values)
 
 rf_model = RandomForestClassifier(random_state=3320)
 rf_model.fit(train_X, train_Y)
 
 plt.bar(feature, rf_model.feature_importances_)
 plt.xticks(rotation='vertical')
-plt.title('Feature Importances')
+plt.title('Feature Importances on predicting HorseWin')
 plt.xlabel('feature')
 plt.ylabel('feature importances')
 plt.tight_layout()
